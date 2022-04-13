c46fecfa74a63f3a9d3f07cd5d2a9c3a500a9fbc
Haopeng Song <jsong2018@icloud.com>
2022-04-13 22:57:56 +0800

    added some features

--- screen/Screen.cpp
+++ screen/Screen.cpp
@@ -20,13 +20,51 @@ public:
     Screen &set(char);
     Screen &set(pos, pos, char);
 
+    // display overloaded on whether the object is const or not
+    // when one member calls another, the 'this' pointer is passed implicitly;
+    // Thus, when display calls do_display, its own 'this' pointer is implicitly passed to do_display;
+    // When the non-const version of display calls do_display, its 'this' pointer is implicitly converted from a pointer to non-const to a pointer to const
+    // When we call display on an object, wheter that object is const determines which version of display is called
+    Screen &display(std::ostream &os)
+    {
+        do_display(os);
+        return *this;
+    }
+    const Screen &display(std::ostream &os) const
+    {
+        do_display(os);
+        return *this;
+    }
+
+    // Window_mgr members can access the private parts of class Screen
+    friend class Window_mgr;
+
+    /*
+
+    Or, Screen can specify that only the clear member is allowed access
+
+    Window_mgr::clear must have been declared before class Screen
+    
+    ///////////////////////////////////////////
+    friend void Window_mgr::clear(ScreenIndex);
+    ///////////////////////////////////////////
+
+    */
+    // ostream version of storeOn may access the private parts of Screen objects
+    friend std::ostream& storeOn(std::ostream &, Screen &);
+
 private:
     mutable size_t access_ctr; // may change even in a const object
     pos cursor = 0;
     pos height = 0, width = 0;
     std::string contents;
-};
 
+    // function do the work of displaying a screen
+    void do_display(std::ostream &os) const
+    {
+        os << contents;
+    }
+};
 
 inline
 Screen &Screen::move(pos r, pos c)
@@ -61,4 +99,8 @@ inline Screen &Screen::set(pos r, pos col, char ch)
     return *this;
 }
 
+// Overloaded storeOn functions
+extern std::ostream& storeOn(std::ostream &, Screen &);
+extern BitMap& storeOn(BitMap &, Screen &);
+      
 
--- screen/Window_mgr.cpp
+++ screen/Window_mgr.cpp
@@ -1,1 +1,37 @@
-﻿
+class Window_mgr {
+public:
+    // Location ID for each screen on the window
+    using ScreenIndex = std::vector<Screen>::size_type;
+    // reset the Screen at the given position to all blanks
+    void clear(ScreenIndex);
+    // add a Screen to the window and returns its index
+    ScreenIndex addScreen(const Screen&);
+    
+private:
+    std::vector<Screen> screens{Screen(24, 80, '')};
+};
+
+void Window_mgr::clear(ScreenIndex i)
+{
+    /*
+
+    If clear (Window_mgr) were not a friend of Screen, this code would not compile.
+
+    The clear function would not be allowed to use the height, width or contents members of Screen.
+    
+    Screen grants friendship to Window_mgr, all the members of Screen are accessible to the functions in Window_mgr.
+
+    */
+    // s is a reference to the Screen we want to clear
+    Screen &s = screens[i];
+    // reset the contents of that Screen to all blanks
+    s.contents = string(s.height * s.width, '');
+}
+
+// return type is seen before we're in the scope of Window_mgr
+// So we must specify the class in which that type is defined
+Window_mgr::ScreenIndex
+Window_mgr::addScreen(const Screen &s) {
+    screens.push_back(s);
+    return screens.size() - 1;
+}
